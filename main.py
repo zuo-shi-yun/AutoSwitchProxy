0fc30648c806fe0b357f1368c5195245e2a838e7
zuo-shi-yun <2818252801@qq.com>
2023-12-16 17:43:08 +0800

    feat-更改切换为rule模式的时机

--- main.py
+++ main.py
@@ -15,11 +15,11 @@ class AutoSwitchProxy(Plugin):
 
     @on(PersonNormalMessageReceived)
     @on(GroupNormalMessageReceived)
-    def handle_normal_msg(self, event: EventContext, **kwargs):
+    def handle_normal_msg_send(self, event: EventContext, **kwargs):
         Clash.global_mode()  # 更改cfw模式——global
 
-    @on(SessionExpired)
-    def handle_session_expired(self, event: EventContext, **kwargs):
+    @on(NormalMessageResponded)
+    def handle_normal_msg_sent(self, event: EventContext, **kwargs):
         Clash.rule_mode()  # 更改cfw模式——rule
 
     # 插件卸载时触发
--- utils/clash.py
+++ utils/clash.py
@@ -7,20 +7,18 @@ import requests
 class Clash:
     @staticmethod
     def rule_mode():
-        Clash.send_request('Rule')
-        logging.info('Switch to Rule Mode')
-        time.sleep(0.5)
+        Clash.send_change_proxy_mode_request('Rule')
 
     @staticmethod
     def global_mode():
-        Clash.send_request('Global')
-        logging.info('Switch to Global Mode')
-        time.sleep(0.5)
+        Clash.send_change_proxy_mode_request('Global')
 
     @staticmethod
-    def send_request(mode: str):
+    def send_change_proxy_mode_request(mode: str):
         secret = '5a6675ec-1201-4596-9469-6b2c2808b956'
         header = {'Authorization': f'Bearer {secret}'}
         body = {'mode': mode}
         res = requests.session().request(headers=header, method='patch', url='http://127.0.0.1:61425/configs',
                                          json=body)
+        time.sleep(0.5)
+        logging.info(f'Switch to {mode} Mode')
