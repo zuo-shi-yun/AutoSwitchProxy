42118d5495e4ef34f9a54d3f47e950053ad79233
zuo-shi-yun <2818252801@qq.com>
2023-12-17 11:51:46 +0800

    feat-插件化

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from pkg.plugin.host import EventContext, PluginHost
 from pkg.plugin.models import *
-from plugins.AutoSwitchProxy.utils.clash import Clash
+from plugins.AutoSwitchProxy.utils.clash import Clash, external_controller, secret
 
 """
 自动更换cfw的proxy模式
@@ -12,6 +12,7 @@ from plugins.AutoSwitchProxy.utils.clash import Clash
 class AutoSwitchProxy(Plugin):
     def __init__(self, plugin_host: PluginHost):
         self.host = plugin_host
+        get_config()  # 获得配置
 
     @on(PersonNormalMessageReceived)
     @on(GroupNormalMessageReceived)
@@ -26,6 +27,17 @@ class AutoSwitchProxy(Plugin):
     # 插件卸载时触发
     def __del__(self):
         pass
+
+
+# 获得clash配置
+def get_config():
+    if isinstance(external_controller, str) and isinstance(secret, str):
+        logging.info("自定义clash配置")
+    else:
+        logging.info("自动获取clash配置")
+        Clash.get_api_config()
+    # 检查配置项
+    Clash.check_clash_api_connect()
 
 
 if __name__ == 'main':
--- utils/clash.py
+++ utils/clash.py
@@ -1,7 +1,15 @@
 import logging
+import os
+import platform
 import time
 
 import requests
+from ruamel.yaml import YAML
+
+# API地址
+external_controller = None
+# API密钥 若为空请改为''
+secret = None
 
 
 class Clash:
@@ -13,12 +21,52 @@ class Clash:
     def global_mode(cls):
         cls.send_change_proxy_mode_request('Global')
 
+    @classmethod
+    def get_api_config(cls):
+        system = platform.system()
+
+        if system != "Windows":
+            raise Exception('自动导入配置仅支持windows系统,请手动设置clash配置')
+
+        user_name = os.environ.get("USERNAME")  # 用户名
+        # 配置文件路径
+        config_path = fr"C:\Users\{user_name}\.config\clash\config.yml"
+
+        # 配置文件yaml
+        try:
+            with open(config_path, 'r', encoding='utf-8') as f:  # 读
+                yamls = YAML(typ='rt')
+                config = yamls.load(f)
+        except Exception as e:
+            raise Exception(f'读取{config_path}文件失败,请检查文件是否存在或是否具有读取权限')
+
+        # 检查配置项
+        if 'external-controller' not in config:
+            raise Exception(f'{config_path}文件中未找到external-controller配置项,请更改配置文件或手动配置')
+        if 'secret' not in config:
+            raise Exception(f'{config_path}文件中未找到secret配置项,请更改配置文件或手动配置')
+
+        # 导入配置
+        global external_controller, secret
+        external_controller = config['external-controller']
+        secret = config['secret']
+        logging.info('clash配置导入成功')
+
+    @classmethod
+    def check_clash_api_connect(cls):
+        try:
+            res = requests.get(f'http://{external_controller}/configs')
+            logging.info('clash api连接正常')
+        except Exception as e:
+            raise Exception(
+                f'clash api连接失败,请检查clash 配置是否正确.当前配置:external-controller:{external_controller},secret:{secret}')
+
     @staticmethod
     def send_change_proxy_mode_request(mode: str):
-        secret = '5a6675ec-1201-4596-9469-6b2c2808b956'
+        global external_controller, secret
         header = {'Authorization': f'Bearer {secret}'}
         body = {'mode': mode}
-        res = requests.session().request(headers=header, method='patch', url='http://127.0.0.1:61425/configs',
+        res = requests.session().request(headers=header, method='patch', url=f'http://{external_controller}/configs',
                                          json=body)
 
         time.sleep(0.5)
